428858626c24805bfafc274c6728d956f6ed42dc
Andrew <Icar08@yandex.ru>
2019-12-27 13:08:32 +0300

    1

---
+++ spring-mvc-demo/src/main/java/com/spring/demo/mvc/TestHibernate.java
@@ -0,0 +1,37 @@
+package com.spring.demo.mvc;
+
+import com.spring.demo.mvc.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class TestHibernate {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
+        try{
+            Student student = new Student("Andrew", "Smirnov", "icar08@yandex.ru");
+            Student student2 = new Student("Mary", "Jane", "MJay@yandex.ru");
+            Student student3 = new Student("Peter", "Parker", "PP@yandex.ru");
+
+            session.beginTransaction();
+
+/*
+            session.save(student);
+            session.save(student2);
+            session.save(student3);
+*/
+            Student retriveStudent = session.get(Student.class, student2.getId());
+            System.out.println(retriveStudent);
+
+            session.getTransaction().commit();
+        }
+        finally {
+            factory.close();
+        }
+    }
+}
