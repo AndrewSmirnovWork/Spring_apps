8e79ce0cdf358fd14fab9f5b7c514f7a1716194a
Andrew <Icar08@yandex.ru>
2020-02-13 15:57:27 +0300

    Spring boot starter

---
+++ SpringBootApp/src/main/java/com/spring/demo/SpringBootApp/rest/RestfulController.java
@@ -0,0 +1,15 @@
+package com.spring.demo.SpringBootApp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class RestfulController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello world! Time on server " + LocalDateTime.now();
+    }
+}
