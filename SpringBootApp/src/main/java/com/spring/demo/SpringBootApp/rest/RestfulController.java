079c583cb70d189006bf18c2d2408fdcb6d2c8de
Andrew <Icar08@yandex.ru>
2020-02-14 16:15:26 +0300

    Spring boot starter

--- SpringBootApp/src/main/java/com/spring/demo/SpringBootApp/rest/RestfulController.java
+++ SpringBootApp/src/main/java/com/spring/demo/SpringBootApp/rest/RestfulController.java
@@ -1,5 +1,6 @@
 package com.spring.demo.SpringBootApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,13 @@ import java.time.LocalDateTime;
 
 @RestController
 public class RestfulController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
 
     @GetMapping("/")
     public String sayHello() {
